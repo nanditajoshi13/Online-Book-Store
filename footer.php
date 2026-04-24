8c0ceef4be505ecc3b43a119e768e9b4305ef0d2
nanditajoshi13 <nanditajoshi001@gmail.com>
2026-04-24 12:08:18 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+</div> <!-- closes container -->
+
+<footer class="bg-dark text-white text-center p-3 mt-5">
+  <p>&copy; 2026 Online Book Store</p>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/script.js"></script>
+</body>
+</html>
