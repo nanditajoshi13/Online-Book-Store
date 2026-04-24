8518b16ceb93a3f2e35a8a6b771348a08e4bdb06
nanditajoshi13 <nanditajoshi001@gmail.com>
2026-04-24 12:09:20 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection settings
+$servername = "localhost";   
+$username   = "root";  
+$password   = "";  
+$dbname     = "bookstore";   
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
